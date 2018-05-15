874a5f5d3c6440b6c2df360779272067c8d4f889
Ryan <ryan.olejnik@gmail.com>
2018-05-15 16:42:14 +0000

    created rough home page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,60 @@
 import React, {Component} from 'react';
 
-class App extends Component {
+function Navbar(props){
+  return (
+    <div id='navbar'>
+      <p>Chattyyyyyyyyyy</p>
+    </div>
+    );
+}
+
+
+class MessageList extends React.Component{
+  render(){
+    return (
+      <div>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+      </div>
+      )
+  }
+}
+
+class Message extends React.Component{
+  render(){
+    return(<p>This is a message</p>);
+  }
+}
+
+class CreateMessageForm extends React.Component{
+  render(){
+    return (
+      <div id='message_form'>
+        <form>
+          <input type='text' name='name' placeholder='Your Name...'></input>
+          <input type='text' name='message' placeholder='Your Message...'></input>
+          <button type='submit'>Post Message!</button>
+        </form>
+      </div>
+      )
+  }
+}
+
+
+class App extends React.Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div id='page_container'>
+        <Navbar/>
+        <MessageList/>
+        <CreateMessageForm/>
+      </div>
     );
   }
 }
+
+
 export default App;
